325bfb4253f63179799cd6d35dcaec56baf28df1
Tiago Borges Nascimento <tiagoborgesnascimento2020@gmail.com>
2022-01-13 22:50:00 -0300

    docker-compose from latest release

--- linux-environment.sh
+++ linux-environment.sh
@@ -28,7 +28,11 @@ apt-cache policy docker-ce;
 sudo apt install -y docker-ce;
 sudo usermod -aG docker ${USER};
 
-sudo apt install -y docker-compose
+
+#docker-compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 #kubernetes
 
